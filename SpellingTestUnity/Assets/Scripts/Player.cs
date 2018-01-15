6bdc61bd1b4456d9c19e057cab42ee8282ba1432
Nathanaël <nathanael.omnes@gmail.com>
2018-01-15 11:35:44 +0000

    Added player movement

---
+++ SpellingTestUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 7;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisX = Input.GetAxis("Horizontal");
+        float axisY = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed);
+    }
+}
