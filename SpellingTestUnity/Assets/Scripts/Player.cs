80133ac36f9b46b72feff6c9ab82bfcab8229269
Nathanaël <nathanael.omnes@gmail.com>
2018-01-20 15:22:34 +0000

    Blocks the player's sprite at the sreen edges

--- SpellingTestUnity/Assets/Scripts/Player.cs
+++ SpellingTestUnity/Assets/Scripts/Player.cs
@@ -9,15 +9,27 @@ public class Player : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get the touch pressed
         float axisX = Input.GetAxis("Horizontal");
         float axisY = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed);
+        
+        //Blocks movement at the edge of the screen
+        if (transform.position.x <= -10.6f)
+            transform.position = new Vector2(-10.6f, transform.position.y);
+        else if (transform.position.x >= 10.6f)
+            transform.position = new Vector2(10.6f, transform.position.y);
+
+        if (transform.position.y <= -4.4f)
+            transform.position = new Vector2(transform.position.x, -4.4f);
+        else if (transform.position.y >= 4.4f)
+            transform.position = new Vector2(transform.position.x, 4.4f);       
     }
 }
