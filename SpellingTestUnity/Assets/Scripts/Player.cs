bbabcbdc0a96bdb8f7d4110e7e49a9efb3a772ae
Nathanaël <nathanael.omnes@gmail.com>
2018-01-28 15:00:36 +0000

    Change sprite when change direction + attack but bug

--- SpellingTestUnity/Assets/Scripts/Player.cs
+++ SpellingTestUnity/Assets/Scripts/Player.cs
@@ -2,28 +2,31 @@
 
 public class Player : MonoBehaviour
 {
-    private float speed = 7;
+    private float speed = 5f;
+
+    private Animator animator;
 
     public GameObject attackParticlePrefab;
     public Transform attackParticleSpawn;
 
-    // Use this for initialization
     void Start()
     {
-
+        animator = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Movement();
 
         if (Input.GetKeyDown(KeyCode.Space))
             Fire();
+        else if (animator.GetBool("Attack") == true)
+            animator.SetBool("Attack", false);
     }
 
     void Fire()
     {
+        animator.SetBool("Attack", true);
         GameObject attackParticle = Instantiate(attackParticlePrefab, new Vector3(transform.position.x, transform.position.y), new Quaternion());
         Destroy(attackParticle, 2f);
     }
@@ -35,6 +38,9 @@ public class Player : MonoBehaviour
         float axisY = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed);
+
+        animator.SetFloat("HorizontalSpeed", axisX);
+        animator.SetFloat("VerticalSpeed", axisY);
 
         //Blocks movement at the edge of the screen
         if (transform.position.x < -4.9f)
