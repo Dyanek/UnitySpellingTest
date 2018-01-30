6dbfc9970436b2bc1468a0b56ecfda219f767f5a
Nathanaël <nathanael.omnes@gmail.com>
2018-01-30 14:02:47 +0000

    Fire wizard movements added
    The movements are not perfect at all

---
+++ SpellingTestUnity/Assets/Scripts/FireWizard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FireWizard : MonoBehaviour
+{
+    private float speed = 5f;
+    private float movementTimer;
+    private float movementCd = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (movementTimer > 0)
+            movementTimer -= Time.deltaTime;
+        else
+        {
+            movementTimer = movementCd;
+            Move();
+        }
+    }
+
+    public void Move()
+    {
+        transform.Translate(new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f)) * Time.deltaTime * speed);
+    }
+}
