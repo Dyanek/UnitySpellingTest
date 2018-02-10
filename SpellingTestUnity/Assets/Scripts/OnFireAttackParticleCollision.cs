26b61b7890841b4d0a10e9621df6b089947b6d5a
Nathanaël <nathanael.omnes@gmail.com>
2018-02-10 16:07:19 +0000

    Added fire wizard attack particle collision

---
+++ SpellingTestUnity/Assets/Scripts/OnFireAttackParticleCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnFireAttackParticleCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "attackParticle")
+            Destroy(gameObject);
+    }
+}
