812df4ced2af378bf185359c69098fa71f4f29b7
yuchingho <yuchingho111@gmail.com>
2018-03-05 15:08:04 +0000

    waterpuzzle

---
+++ SpellingTestUnity/Assets/Scripts/WaterPuzzle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaterPuzzle : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+    public GameObject rockPrefab;
+    private float timeToSpawn = 4f;
+    public float timeBetweenWaves = 1f;
+
+    public string LeveltoLoad;
+
+    // Use this for initialization
+    void Update ()
+    {
+        if (Time.time >= timeToSpawn)
+        {
+            SpawnRocks();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+
+        if (Time.time >= 37)
+        {
+            Debug.Log("Time up");
+            SceneManager.LoadScene(LeveltoLoad);
+        }
+	}
+
+    void SpawnRocks()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(rockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
