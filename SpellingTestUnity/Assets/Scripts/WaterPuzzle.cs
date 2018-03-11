631490977d873448ea74d605fc7fc31e5abc7bae
Dyanek <nathanael.omnes@gmail.com>
2018-03-11 18:50:29 +0000

    Solved water puzzle bug

--- SpellingTestUnity/Assets/Scripts/WaterPuzzle.cs
+++ SpellingTestUnity/Assets/Scripts/WaterPuzzle.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WaterPuzzle : MonoBehaviour {
-
+public class WaterPuzzle : MonoBehaviour
+{
     public Transform[] spawnPoints;
     public GameObject rockPrefab;
-    private float timeToSpawn = 4f;
+    private float timeToSpawnTimer;
     public float timeBetweenWaves = 1f;
 
-    public string LeveltoLoad;
+    public string LevelToLoad;
 
-    // Use this for initialization
-    void Update ()
+    void Update()
     {
-        if (Time.time >= timeToSpawn)
+        if (timeToSpawnTimer > 0)
+            timeToSpawnTimer -= Time.deltaTime;
+        else
         {
             SpawnRocks();
-            timeToSpawn = Time.time + timeBetweenWaves;
+            timeToSpawnTimer = timeBetweenWaves;
         }
 
-        if (Time.time >= 37)
+        if (Time.timeSinceLevelLoad >= 37)
         {
             Debug.Log("Time up");
-            SceneManager.LoadScene(LeveltoLoad);
+            SceneManager.LoadScene(LevelToLoad);
         }
-	}
+    }
 
     void SpawnRocks()
     {
@@ -35,9 +36,7 @@ public class WaterPuzzle : MonoBehaviour {
         for (int i = 0; i < spawnPoints.Length; i++)
         {
             if (randomIndex != i)
-            {
                 Instantiate(rockPrefab, spawnPoints[i].position, Quaternion.identity);
-            }
         }
     }
 }
